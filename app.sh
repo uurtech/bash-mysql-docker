bfe8aba4bd35d94f13d2e64afd3aa16cfd5fd369
uurtech <girisiyorum@gmail.com>
2024-01-14 21:18:25 +0300

    fixed mysql query

--- app.sh
+++ app.sh
@@ -9,9 +9,9 @@ while true; do
     random_number=$((1 + $RANDOM % 10))
 
 
-    export NEW_QUERY="${QUERY}${random_number};"
+    export NEW_QUERY="${QUERY} ${random_number};"
     echo $NEW_QUERY
-    mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -D $MYSQL_DATABASE -e "$QUERY"
+    mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -D $MYSQL_DATABASE -e "$NEW_QUERY"
     sleep 1  # Wait for 1 second before running the query again
 done
 tail -f /dev/null
