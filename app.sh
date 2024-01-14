94021cf0ad7cb6f9222e3d98b1a96f100920ec8d
uurtech <girisiyorum@gmail.com>
2024-01-14 21:08:20 +0300

    query deki app.sh hatası giderildi.

--- app.sh
+++ app.sh
@@ -7,12 +7,12 @@ MYSQL_PASSWORD=rootpassword
 MYSQL_DATABASE=mydatabase
 
 # Example query
-QUERY="SELECT * FROM awesome_data limit 1;"
+QUERY="SELECT name,description FROM awesome_data limit 1;"
 echo "test"
 sleep 10
 # Connect to MySQL and run the query
 while true; do
-    mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -D $MYSQL_DB -e "$QUERY"
+    mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -D $MYSQL_DATABASE -e "$QUERY"
     sleep 1  # Wait for 1 second before running the query again
 done
 tail -f /dev/null
